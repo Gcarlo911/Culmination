9950467e9a318a9f961588cfeb5e9d8ea5e62aea
Gcarlo911 <gcarlo911@gmail.com>
2020-10-10 23:49:39 -0400

    more progress
    worked on enemies and collision

---
+++ CulminationUnity/Assets/Scripts/AestroidMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AestroidMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = this.transform.position;
+        position.x = position.x - .05f;
+        this.transform.position = position;
+        transform.Rotate(0, 0, 100 * Time.deltaTime);
+
+        
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hiiii");
+        Destroy(this.gameObject);
+    }
+
+}
