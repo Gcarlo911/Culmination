cf080d250110d6e211e7c2b0846b1128f0a82272
Gcarlo911 <gcarlo911@gmail.com>
2020-10-19 17:52:59 -0400

    spawn works

--- CulminationUnity/Assets/Scripts/AestroidMove.cs
+++ CulminationUnity/Assets/Scripts/AestroidMove.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class AestroidMove : MonoBehaviour
 {
+
+    private GameManager manager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -23,7 +25,13 @@ public class AestroidMove : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("hiiii");
+        if(collision.gameObject.tag == "Player")
+        {
+            //variable health stuff
+            manager.DescreseHealth();
+
+        }
+        
         Destroy(this.gameObject);
     }
 
---
+++ CulminationUnity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject planetPrefab;
+    public GameObject asteroidPrefab;
+    public GameObject commetPrefab;
+    private float xSpawn = 10f;
+    private float ySpawnMin = 3.8f;
+    private float ySpawnMax = -3.8f;
+    private float timeTillSpawn;
+    private float spawnDelay=2;
+    private int health = 100;
+    public TextMeshProUGUI healthText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeTillSpawn =spawnDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        healthText.text =  "Health: "+health.ToString();
+
+
+        if (timeTillSpawn <= 0)
+        {
+            Vector3 pos = new Vector3(xSpawn,Random.Range(ySpawnMin,ySpawnMax),0);
+            Instantiate(asteroidPrefab,pos,Quaternion.identity);
+            timeTillSpawn = spawnDelay;
+        }
+        else
+        {
+            timeTillSpawn -= Time.deltaTime;
+        }
+    }
+
+    public void DescreseHealth()
+    {
+        health -= 1;
+    }
+
+}
+
+
