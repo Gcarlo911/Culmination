4f4b7423e267b1830d3bf087010b5cb23aecc6c1
Gcarlo911 <gcarlo911@gmail.com>
2020-10-27 20:15:22 -0400

    proggy
    coomeett

--- CulminationUnity/Assets/Scripts/AestroidMove.cs
+++ CulminationUnity/Assets/Scripts/AestroidMove.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class AestroidMove : MonoBehaviour
 {
+    public enum EnemyType
+    {
+        Planet,
+        Asteroid,
+        Comet
+    }
 
+    public EnemyType enemyType;
+
+    public float speed =2f;
+    public int rotation = 100;
+    public int damage = 1;
+    
     private GameManager manager;
     // Start is called before the first frame update
     void Start()
     {
         manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        //if (gameObject.tag == "Planet") isPlanet = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 position = this.transform.position;
-        position.x = position.x - .05f;
-        this.transform.position = position;
-        transform.Rotate(0, 0, 100 * Time.deltaTime);
+      //  Vector3 position = this.transform.position;
+      //  position.x = position.x - .05f;
+        float movement = -1 * speed * Time.deltaTime;
+        Vector3 xpos = new Vector3(movement, 0, 0);
+        this.transform.position +=xpos;
+        transform.Rotate(0, 0, rotation * Time.deltaTime);
 
         
 
@@ -27,12 +43,52 @@ public class AestroidMove : MonoBehaviour
     {
         if(collision.gameObject.tag == "Player")
         {
-            //variable health stuff
-            manager.DescreseHealth();
-
+            manager.DescreseHealth(damage);
+            //Destroy(this.gameObject);
         }
-        
-        Destroy(this.gameObject);
+            
+        if(collision.gameObject.tag == "LeftBoundery")
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (enemyType == EnemyType.Planet)
+        {
+            if (collision.gameObject.tag == "Asteroid" || collision.gameObject.tag == "Commet")
+            {
+
+                Destroy(collision.gameObject);
+            }
+
+            if (collision.gameObject.tag == "Player")
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        if (enemyType == EnemyType.Asteroid)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (enemyType == EnemyType.Comet)
+        {
+            if (collision.gameObject.tag == "Asteroid")
+            {
+
+                Destroy(collision.gameObject);
+            }
+
+            if (collision.gameObject.tag == "Player")
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+
+
+
+        //Destroy(this.gameObject);
     }
 
 }
--- CulminationUnity/Assets/Scripts/GameManager.cs
+++ CulminationUnity/Assets/Scripts/GameManager.cs
@@ -8,19 +8,33 @@ public class GameManager : MonoBehaviour
     public GameObject planetPrefab;
     public GameObject asteroidPrefab;
     public GameObject commetPrefab;
-    private float xSpawn = 10f;
-    private float ySpawnMin = 3.8f;
-    private float ySpawnMax = -3.8f;
-    private float timeTillSpawn;
-    private float spawnDelay=2;
+    private float xSpawn = 12f;
+    private float ySpawnMin = 5f;
+    private float ySpawnMax = -5f;
+
+    private float asteroidTimeTillSpawn;
+    private float asteroidSpawnDelay = 1;
+
+    private float planetTimeTillSpawn;
+    private float planetSpawnDelay = 15;
+
+    private float commetTimeTillSpawn;
+    private float commetSpawnDelay = 8;
+
+    private float asteroidInitialSpawnDelay=0;
+    private float planetInitialSpawnDelay=15;
+    private float commetInitialSpawnDelay=25;
+
     private int health = 100;
     public TextMeshProUGUI healthText;
     // Start is called before the first frame update
     void Start()
     {
-        timeTillSpawn =spawnDelay;
+        asteroidTimeTillSpawn = asteroidInitialSpawnDelay;
+        planetTimeTillSpawn = planetInitialSpawnDelay;
+        commetTimeTillSpawn = commetInitialSpawnDelay;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -28,21 +42,45 @@ public class GameManager : MonoBehaviour
         healthText.text =  "Health: "+health.ToString();
 
 
-        if (timeTillSpawn <= 0)
+        if (asteroidTimeTillSpawn <= 0)
         {
             Vector3 pos = new Vector3(xSpawn,Random.Range(ySpawnMin,ySpawnMax),0);
             Instantiate(asteroidPrefab,pos,Quaternion.identity);
-            timeTillSpawn = spawnDelay;
+            asteroidTimeTillSpawn = asteroidSpawnDelay;
         }
         else
         {
-            timeTillSpawn -= Time.deltaTime;
+            asteroidTimeTillSpawn -= Time.deltaTime;
+        }
+        //////////////////
+
+        if (planetTimeTillSpawn <= 0)
+        {
+            Vector3 pos = new Vector3(xSpawn, Random.Range(ySpawnMin, ySpawnMax), 0);
+            Instantiate(planetPrefab, pos, Quaternion.identity);
+            planetTimeTillSpawn = planetSpawnDelay;
+        }
+        else
+        {
+            planetTimeTillSpawn -= Time.deltaTime;
+        }
+        /////////////////////
+
+        if (commetTimeTillSpawn <= 0)
+        {
+            Vector3 pos = new Vector3(xSpawn, Random.Range(ySpawnMin, ySpawnMax), 0);
+            Instantiate(commetPrefab, pos, transform.rotation * Quaternion.Euler(0, 0, -35));
+            commetTimeTillSpawn = commetSpawnDelay;
+        }
+        else
+        {
+            commetTimeTillSpawn -= Time.deltaTime;
         }
     }
 
-    public void DescreseHealth()
+    public void DescreseHealth(int value)
     {
-        health -= 1;
+        health -= value;
     }
 
 }
